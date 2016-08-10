166786f288a2c96f4752695fc7e41974c4bcaeb4
Melvin Canela <M.Canela92@outlook.com>
2016-08-10 18:54:15 -0400

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+  jade = require('gulp-jade'),
+  sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  sourcemaps = require('gulp-sourcemaps'),
+  changed = require('gulp-changed'),
+  gutil = require('gulp-util'),
+  browserify = require('browserify'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload
+
+gulp.task('serve', () => {
+  browserSync({
+    server: {
+      baseDir: './production'
+    }
+  })
+})
+
+gulp.task('watch', ['jade', 'sass', 'js'], ()=>{
+  gulp.watch('./src/*.jade', ['jade'])
+  gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch('./src/js/**/*.js', ['js'])
+})
+
+gulp.task('jade', () => {
+  return gulp.src('./src/*.jade')
+    .pipe(changed('./production/'))
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest('./production/'))
+    .pipe(reload({ stream: true }))
+})
+
+gulp.task('sass', () => {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(changed('./production/css/'))
+    .pipe(sass().on('error', gutil.log))
+    .pipe(gulp.dest('./production/css/'))
+    .pipe(reload({ stream: true }))
+})
+
+gulp.task('js', () => {
+  let b = browserify({
+    entries: './src/js/app.js',
+    debug: true
+  })
+
+  return b.bundle()
+    .pipe(gulp.src('./src/js/**/*.js'))
+    .pipe(changed('./production/js/'))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./production/js/'))
+    .pipe(gulp.dest('./production/js/'))
+})
