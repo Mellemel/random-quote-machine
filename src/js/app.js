a794dbd9ecf4b190c085ef97a99df730d8a73bba
Melvin Canela <M.Canela92@outlook.com>
2016-08-15 01:07:22 -0400

    new quote button now functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,44 @@
 global.jQuery = require('jquery')
 require('./typed.js')
 
-jQuery(() => {
+jQuery(document).ready(() => {
+  jQuery.ajaxSetup({ cache: false })
+  getQuote(writeQuote)
 
-  jQuery.getJSON('https://crossorigin.me/http://api.forismatic.com/api/1.0/', {
+  jQuery('.primary').click(() => {
+    jQuery('.typed-cursor').remove()
+    jQuery('.quote, .author').empty()
+    getQuote(writeQuote)
+  })
+})
+
+function getQuote(cb) {
+  var url = 'https://crossorigin.me/http://api.forismatic.com/api/1.0/'
+
+  var options = {
     method: 'getQuote',
     format: 'json',
     lang: 'en'
-  },
-    (data) => {
-      var quote = data.quoteText, author = data.quoteAuthor || 'Unknown'
+  }
 
-      console.log(quote.length)
-      var typedOptions = {
-        strings: [quote],
-        typeSpeed: 20,
-        contentType: 'text',
-        callback: () => {
-          typedOptions.strings = ['- '+ author]
-          typedOptions.showCursor = false
-          delete typedOptions.callback
-          jQuery('.author').typed(typedOptions)
-        }
-      }
-      jQuery('.quote').typed(typedOptions)
-    })
-})
+  jQuery.getJSON(url, options, (data)=>{
+    cb(data)
+  })
+}
+
+function writeQuote(data) {
+  var quote = data.quoteText, author = data.quoteAuthor || 'Anonymous'
+
+  let typedOptions = {
+    strings: ['"'+quote+'"'],
+    typeSpeed: 50,
+    contentType: 'text',
+    callback: () => {
+      typedOptions.strings = ['- ' + author]
+      jQuery('.typed-cursor').remove()
+      delete typedOptions.callback
+      jQuery('.author').typed(typedOptions)
+    }
+  }
+  jQuery('.quote').typed(typedOptions)
+}
